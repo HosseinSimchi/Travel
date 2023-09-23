1ee2d4f24dc15138677f6800a48d81e8ed5e1fd3
= <=>
2023-09-23 15:00:43 +0330

    added some files

---
+++ travel/src/app/blog/blog.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { SingleComponent } from './single/single.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    SingleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
---
+++ travel/src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElementsComponent } from './elements/elements.component';
+
+
+
+@NgModule({
+  declarations: [
+    ElementsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
